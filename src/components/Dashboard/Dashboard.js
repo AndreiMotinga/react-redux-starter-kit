4d9742597e5102034e5fb137c366519c3cad3d4f
Andrei Motinga <andrei@Andreis-MacBook-Air.local>
2017-02-10 18:38:55 -0500

    adds visits counter do a dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -4,26 +4,17 @@ import classes from './Dashboard.scss'
 export const Dashboard = (props) => (
   <div>
     <h2 className={classes.counterContainer}>
-      Dashboard:
+      Dashboard visits:
       {' '}
       <span className={classes['counter--green']}>
         {props.dashboard}
       </span>
     </h2>
-    <button className='btn btn-default' onClick={props.increment}>
-      Increment
-    </button>
-    {' '}
-    <button className='btn btn-default' onClick={props.doubleAsync}>
-      Double (Async)
-    </button>
   </div>
 )
 
 Dashboard.propTypes = {
   dashboard: React.PropTypes.number.isRequired,
-  doubleAsync: React.PropTypes.func.isRequired,
-  increment: React.PropTypes.func.isRequired
 }
 
 export default Dashboard
